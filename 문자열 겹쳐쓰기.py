d62f1a8a38714126360dadc9a5ffcfd54b7840b0
jeongdw1001 <ja40476@gmail.com>
2023-05-23 01:14:18 +0900

    Lev 0

---
+++ 문자열 겹쳐쓰기.py
@@ -0,0 +1,11 @@
+my_string, overwrite_string, s = map(str, input().strip().split())
+
+# def solution(my_string, overwrite_string, s):
+#     answer = ''
+#     return answer
+
+
+
+print(my_string[2:5])
+print(overwrite_string)
+print(s)
---
+++ 홀짝 구분하기.py
@@ -0,0 +1,16 @@
+# 내 풀이
+n = int(input())
+
+if n % 2 == 0:
+    print(n,"is even")
+else:
+    print(n,"is odd")
+
+# 다른 풀이
+a = int(input())
+
+if a%2 == 0:
+    print(f'{a} is even')
+
+else:
+    print(f'{a} is odd')
