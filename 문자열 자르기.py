6419667d42ad377124c75ad375a2c4d7dcf4cc0c
jeongdw1001 <ja40476@gmail.com>
2023-05-23 00:44:56 +0900

    Day 1 update

---
+++ 문자열 자르기.py
@@ -0,0 +1,23 @@
+# 내 풀이
+str = input()
+for i in range(0, len(str), 1):
+    print(str[i])
+
+# 다른 풀이
+print('\n'.join(input()))
+
+# 다른 풀이2
+for a in input():
+    print(a)
+
+
+# 입력 
+# abcde
+
+
+# 출력 
+# a
+# b
+# c
+# d
+# e
