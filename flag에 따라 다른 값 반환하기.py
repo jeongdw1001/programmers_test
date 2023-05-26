06d2308a42dd81b9fe99c8a199f98203ba8ab84d
jeongdw1001 <ja40476@gmail.com>
2023-05-26 11:39:30 +0900

    20230525 study

---
+++ flag에 따라 다른 값 반환하기.py
@@ -0,0 +1,20 @@
+# 문제 설명
+# 두 정수 a, b와 boolean 변수 flag가 매개변수로 주어질 때, 
+# flag가 true면 a + b를 false면 a - b를 return 하는 solution 함수를 작성해 주세요.
+
+# 내 풀이
+def solution(a, b, flag):
+    if flag == True:
+        return a + b
+    else:  
+        return a - b
+    
+# 다른 풀이
+def solution(a, b, falg):
+    if flag:
+        return a + b
+    return a - b
+
+# 다른 풀이2
+def solution(a, b, flag):
+    return a + b if flag else a - b
