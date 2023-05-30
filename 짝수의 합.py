afad7dbfcd16a4d73395b4c53a714bae72a929c6
jeongdw1001 <ja40476@gmail.com>
2023-05-30 16:13:04 +0900

    20230530 study

--- 짝수의 합.py
+++ 짝수의 합.py
@@ -5,4 +5,18 @@ def solution(n):
         answer += i
     return answer
 
-# 다른 풀이
+# 다른 풀이1
+def solution(n):
+    return sum([i for i in range(2, n+1, 2)])
+
+# 다른 풀이2
+def solution(n):
+    return 2*(n//2)*((n//2)+1)/2
+
+# 다른 풀이3
+def solution(n):
+    answer = 0
+    for i in range(1, n+1):
+        if i % 2 == 0:
+            answer += 1
+    return answer
