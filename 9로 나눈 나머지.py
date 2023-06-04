b2f3498745e1874bd78fb370465e40f689bb88be
jeongdw1001 <ja40476@gmail.com>
2023-06-04 23:51:46 +0900

    lv0 20230604 study

--- 9로 나눈 나머지.py
+++ 9로 나눈 나머지.py
@@ -3,3 +3,11 @@
 def solution(number):
     a = list(map(int, str(number)))
     return sum(a) % 9
+
+# 다른 풀이 1
+def solution(number):
+    return int(number) % 9
+
+# 다른 풀이 2
+def solution(number):
+    return sum([int(i) for i in number]) % 9
